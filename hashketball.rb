7e3f992c2be9b6d692af0229e4acb26627c6337e
Isak Kallenbach <kallenbach13@gmail.com>
2020-03-04 13:31:31 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -190,3 +190,17 @@ def player_stats(players_name)
   end
   new_hash
 end
+
+def big_shoe_rebounds
+  big_shoe = 0
+  rebounds = 0
+  game_hash.each do |place, team|
+    team[:players].each do |player|
+      if player[:shoe] > big_shoe
+        big_shoe = player[:shoe]
+        rebounds = player[:rebounds]
+      end
+    end
+  end
+  return rebounds
+end
